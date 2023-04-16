444cde421afe2e64c444fbf07733a6594b92aab7
Anastasiia Sedalishcheva <ffttyyll@gmail.com>
2023-04-16 20:32:16 +0900

    update

--- index.js
+++ index.js
@@ -1,53 +1,65 @@
-let avatar= document.querySelector('.avatar');
-
 let radio = document.getElementById('checkNo');
-
+let avatar = document.querySelector('.avatar');
 let date = new Date().toLocaleString()
+console.log(date);
 
 
-let ava = document.getElementById('avatar').value;
+
+
 function changeImg() {
+    let ava = document.getElementById('avatar').value;
 if (ava === "")
-    {let randomAva = Math.round(Math.random()*4);
+    {let randomAva = Math.round(Math.random()*5);
     console.log(randomAva);
-if (randomAva = 0){
-    avatar.src = "img/1.webp"
-}
-if(randomAva = 1){
-    avatar.src = "img/2.webp"
-}
-if(randomAva = 2){
-    avatar.src = "img/3.webp"
-}if(randomAva = 3){
-    avatar.src = "img/4.webp"
-}if(randomAva = 4){
+    if (randomAva === 0){
+        avatar.src = "img/1.webp"
+    }
+    else if(randomAva === 1){
+        avatar.src = "img/2.webp"
+    }
+    else if(randomAva === 2){
+        avatar.src = "img/3.webp"
+    }
+    else if(randomAva == 3){
+        avatar.src = "img/4.webp"
+    }
+    else if(randomAva === 4){
     avatar.src = "img/5.jpg"
-}}
-else {
+    }
+} else {
     avatar.src= ava;
 }
 }
 
 
 function nameMy() {
+    
     let MyName = document.getElementById('name').value;
     let small = MyName.toLowerCase();
-    document.querySelector('#result').innerHTML = small;
-
+    
+    let radio = document.getElementById('checkNo');
     if (radio.checked){
         small = "Gest";
     }
-    
+    document.querySelector('#result').innerHTML = small;
 }
-let text = document.getElementById('comment').value;
-function Com(){
-    
-    let text= text.replace(/XXX/gi, "***");
-    document.querySelector('#chat').innerHTML = text;
-    
-}
-console.log(text);
 
-button.addEventListener('click', changeImg)
-button.addEventListener('click', nameMy)
-button.addEventListener('click', Com)
+
+
+function Com(){
+    let text = document.getElementById('comment').value;
+    text = text.replace(/XXX/gi, "***");
+    document.querySelector('#chat').innerHTML = text;
+    console.log(text)}
+const block = document.querySelector(".block");
+const button = document.getElementById("button");
+let total = "";
+button.addEventListener('click', changeImg);
+button.addEventListener('click', nameMy);
+button.addEventListener('click', () => {
+    total = total + `<img src="${ava}" class="avatar" width="100px" alt="avatar"><p id="date">${date}</p><p id="result">${small}</p>
+    <p id="chat">${text}</p> `;
+});
+
+
+
